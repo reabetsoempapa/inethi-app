fe6619f2250015b79448e54321c49baee3158201
Takunda <dzingietakue@gmail.com>
2024-09-15 23:55:20 +0200

    fixed conflicts

--- inethi/App.js
+++ inethi/App.js
@@ -58,11 +58,7 @@ const HomePageStack = ({ logout }) => {
         name="RequestNode"
         options={{
           header: ({ navigation }) => (
-<<<<<<< HEAD
-            <AppBarComponent title="App Store" logout={logout} />
-=======
             <AppBarComponent title="Installation" logout={logout} />
->>>>>>> 6c22626 (OtherChanges)
           ),
         }}
       >
@@ -71,13 +67,7 @@ const HomePageStack = ({ logout }) => {
       <Stack.Screen
         name="HotspotOptions"
         options={{
-<<<<<<< HEAD
-          header: ({ navigation }) => (
-            <AppBarComponent title="Web View" logout={logout} />
-          ),
-=======
           headerShown: false, // Disable header here
->>>>>>> 6c22626 (OtherChanges)
         }}
       >
         {props => <HotspotOptionsPage {...props} logout={logout} />}
@@ -91,6 +81,17 @@ const HomePageStack = ({ logout }) => {
         }}
       >
         {props => <MapPage {...props} logout={logout} />}
+      </Stack.Screen>
+      {/* Add the AppStore screen here */}
+      <Stack.Screen
+        name="AppStore"
+        options={{
+          header: ({ navigation }) => (
+            <AppBarComponent title="App Store" logout={logout} />
+          ),
+        }}
+      >
+        {props => <FdroidAppstore {...props} logout={logout} />}
       </Stack.Screen>
       <Stack.Screen
         name="ReviewPage"
